6ad000b63d8f1adaf02fab7bfe0f4877c41dc94a
Muramasa <haydenkilloh@gmail.com>
2020-09-18 00:29:52 +0100

    Documentation Splitting the code to elm and assign Implementing the base class component and events

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-class Card {
-    constructor(d) {
-        this.e = e(`#${d}`);
-    }
-    update(d) {
-        this.e.innerHTML = d;
-    }
-    onFrag(p) {
-        console.log("Fragged onto:", p);
+class Card extends Comp {
+    constructor(s, a) {
+        super(s, a);
+        this.i = 0;
+        assign(this.e, {onclick:()=>this.reFrag()});
+        //this.e["onclick"] = () => this.reFrag();
+        this.onFrag = () => {
+            this.i++;
+            this.e.innerHTML = "Nebula Lib " + this.i;
+        };
     }
 }
 
 on(d, "DOMContentLoaded", _ => {
     on(w, "load", _ => console.log("load"));
 
-    frag($("#d"),
-        e('x', 'x', {style:{color:"red"}}, e('y', 'y', {style:{color:"green"}}, e('z', 'z', {style:{color:"blue"}})))
-    );
+    //frag($("#d"),
+        //e('test#testid.testclass', 'x', {style:{color:"red"}}, e('y', 'y', {style:{color:"green"}}, e('z', 'z', {style:{color:"blue"}})))
+    //);
 
-    let card = new Card("nebula");
-    card.update("Nebula Lib");
+    frag($("#d"), elm('div#brandon.cuck', 'lmao', {style:{color:"red"}, onclick:function(){alert("test")}}));
+
+    let card = new Card("nebula", {style:{color:"red"}});
     frag($('#d'), card);
 });
--- js/nebula.js
+++ js/nebula.js
@@ -1,36 +1,55 @@
-const cache = {}, df = document.createDocumentFragment();
+const cache = {}, df = document.createDocumentFragment(); //The node creation cache and document frag
 
-function e(s, ...a) {
-    let e, q = s;
-    if ((e = cache[s])) return e.cloneNode(false);
-    s = [(s = s.split('.'))[0].split('#'), s[1]];
-    e = document.createElement(s[0][0] || 'div');
-    if (s[0][1]) e.id = s[0][1];
-    if (s[1]) e.className = s[1];
-    if (a) {
-        let b, type;
-        for (let i = 0; i < a.length; i++) {
-            b = a[i]; type = typeof b;
-            if (b&& b.nodeType) e.appendChild(b);
-            else if (type === 'object') {
-                for (let j in b) {
-                    if (j === 'style') for (let k in b.style) e.style[k] = b.style[k];
-                    else e[j] = b[j];
-                }
-            }
-            else if (type === 'string' || type === 'number') e.textContent = b;
-            else if (type === 'function') b(e);
-        }
+class Comp {
+    constructor(s, a) { //Copies the args for e(), to pass directly
+        this.e = elm(s, a);
+        this.frag = (p) => this.onFrag(this.p = p);
+        this.reFrag = () => frag(this.p, this);
+        this.onFrag = () => {};
     }
-    return (cache[q] = e);
 }
 
-function frag(p, c) {
-    if (c.e) {
-        df.appendChild(c.e);
-        if (c.onFrag) c.onFrag(p);
-    } else {
-        df.appendChild(c);
+function elm(s, ...a) {
+    let e, q = s; //Define e (our element to build) and a copy of s as q (to preserve the original query before the split)
+    if ((e = cache[s])) return e.cloneNode(false); //If The node we're trying to build exists in the cache, return a copy
+    s = [(s = s.split('.'))[0].split('#'), s[1]]; //Split s to get tag, id and class ids from queries (tag#testId.testClass)
+    e = document.createElement(s[0][0] || 'div'); //Create the node from the query data, as a div tag?
+    if (s[0][1]) e.id = s[0][1]; //If the query[0][1] has an entry for id, assign it
+    if (s[1]) e.className = s[1]; //If the query[1] has an entry for className, assign it
+    if (a) assign(e, ...a); //If extra data was passed into the function
+    return (cache[q] = e); //Finally put our new node in the cache and return it
+}
+
+
+function assign(node) {
+    let b, type; //Save a copy of the i'th element as b, and the type of b for efficency
+    for (let i = 1; i < arguments.length; i++) { //Loop over arguments starting at 1 to skip node
+        b = arguments[i]; type = typeof b; //Assign efficnecy variables
+        if (b && b.nodeType) node.appendChild(b); //If b is a node, append b to our newly constructed node to nest it
+        else if (type === 'object') { //If we were passed an object, assign properties to our new node
+            for (let j in b) { //Loop over properties
+                if (j === 'style') { //The root object for css data must begin with a style -> object mapping
+                    for (let k in b.style) { //Loop over all the entries inside the style object
+                        node.style[k] = b.style[k]; //For every entry in the style object, assign it to our new node
+                    }
+                } else {
+                    node[j] = b[j]; //Otherwise, assign node properties to the data passed. This could be onclick: function(){alert(test)}
+                }
+            }
+        }
+        else if (type === 'string' || type === 'number') node.textContent = b; //If data is alphanumeric, assign it to text content
+        else if (type === 'function') b(node); //If data is a function, treat it as a callback and pass our new node
     }
-    p.appendChild(df);
+}
+
+
+
+function frag(p, c) {
+    if (c.e) { //If passed a object with a member of e assigned, assume to be a component and access the node
+        df.appendChild(c.e); //Append to document frag
+        if (c.frag) c.frag(p); //If the component has a onFrag method defined, call it
+    } else {
+        df.appendChild(c); //Otherwise, assume we got a normal node and append it to the document frag
+    }
+    if (p) p.appendChild(df); //Finally append the node(s) in the frag to the parent element if it is assigned
 }
